d9add933ed9ecd85c79cb004b481f1f6dfa020f2
Clint <masterplinko@gmail.com>
2014-10-17 03:24:59 -0700

    create encounter.h

---
+++ encounter.h
@@ -0,0 +1,19 @@
+#ifndef ENCOUNTER_H
+#define ENCOUNTER_H
+
+struct stats
+    {
+    int attack;
+    int maxdefense;
+    int defense;
+    int hp;
+    int maxhp;
+	int armor;
+	int mp;
+    int maxmp;
+    };
+
+
+stats encounter();
+
+#endif
