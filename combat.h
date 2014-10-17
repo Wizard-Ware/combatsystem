949e8802850915c29c76e2a92e92cdd1499bd6a9
Clint <masterplinko@gmail.com>
2014-10-17 03:23:34 -0700

    Create combat.h

---
+++ combat.h
@@ -0,0 +1,8 @@
+#ifndef COMBAT_H
+#define COMBAT_H
+
+int combat_system_mob (int atk, int armor, int health, int def, int maxhp);
+
+int combat_system_player (int atk, int armor, int health, int def, int maxhp);
+
+#endif
