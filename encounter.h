82c6a7e6feadfb407e430babf0f8c9ac13da35d9
Clint <masterplinko@gmail.com>
2014-10-18 04:40:42 -0700

    update encounter.h
    added comments

--- encounter.h
+++ encounter.h
@@ -1,6 +1,8 @@
 #ifndef ENCOUNTER_H
 #define ENCOUNTER_H
 
+//Basic struct of type stats used as the basis for 
+//all monster and player stats
 struct stats{
     int attack;
     int maxdefense;
@@ -12,7 +14,12 @@ struct stats{
     int maxmp;
     };
 
-
+/**
+ * Generates a random number between 1 - 3 to set what monster attacks the player
+ * @param: There are no parameters taken in for this function
+ * @return: monster stats in the form of a struct of type stats
+ * */
+ 
 stats encounter();
 
 #endif
