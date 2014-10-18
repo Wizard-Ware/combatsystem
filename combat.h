2965b5e9819b8fca07e12eb1391094c0746e1744
Clint <masterplinko@gmail.com>
2014-10-18 04:47:22 -0700

    update combat.h
    added more commenting to explain functions

--- combat.h
+++ combat.h
@@ -11,10 +11,20 @@ struct magic{
 	int ward_cost;
 };
 
-
-
+/**
+ * Rolls a random number from 1 - 20 to check if the mob hits player and applies damaged based
+ * on the stats of the mob. 
+ * @param: attack of monster and armor health def and maxhp of player
+ * @return: health of player
+ * */
 int combat_system_mob(int atk, int armor, int health, int def, int maxhp);
 
+/**
+ * Rolls a random number from 1 - 20 to check if player hits mob and applies damage based
+ * on the stats of mob
+ * @param: attack of player and armor, health, def, and maxhp of mob
+ * @return: health of mob
+ * */
 int combat_system_player(int atk, int armor, int health, int def, int maxhp);
 
 #endif
