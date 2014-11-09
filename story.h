fbbeed264177db34ca3cd8920e387e5faa7fa350
Clint <masterplinko@gmail.com>
2014-11-09 01:47:55 -0800

    Create story.h

---
+++ story.h
@@ -0,0 +1,14 @@
+#ifndef STORY_H
+#define STORY_H
+
+/*****************************************************************************************************
+**Function: story()
+**Description: This takes in two integers and reads from a txt file called "story.txt"
+** It will print lines from it to give the story of the game so far.
+**Parameters: two integers one for the start of printing the other for the last line to print
+**Pre-Conditions: This will be called by to manually put in integers placing this around the game
+**Post-Conditions: Once the story lines have been printed this fuction will end and return no values
+*****************************************************************************************************/
+void story(int chapstart, int chapend);
+
+#endif
