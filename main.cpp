5df6aceb7b95486a32a3c26544bc538c38fc8a7a
Clint <masterplinko@gmail.com>
2014-11-10 08:45:32 -0800

    implementing start of magic system

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 #include "encounter.h"
 #include "combat.h"
 #include "story.h"
+#include "magic.h"
 using std::cout;
 using std::endl;
 using std::cin;
@@ -22,7 +23,7 @@ int main(){
 	int spell;		//used in the spell menu switch.
 	int damage;		//holds damage to be subtracted from hp.
 
-	stats mob;{
+	stats mob;
 		mob.attack = 0;			//attack damage
 		mob.maxdefense = 0;		//max defense for a reset defense after use of ward.
 		mob.defense = 0;		//defense.
@@ -31,9 +32,8 @@ int main(){
 		mob.armor = 0;			//subtracts from damage before it is applied to hp.
 		mob.mp = 0;			//magic points.
 		mob.maxmp = 0;			//maximum magic points a creature can have.
-	};
 
-	stats player;{
+	stats player;
 		player.attack = 5;
 		player.maxdefense = 5;
 		player.defense = 2;
@@ -42,16 +42,15 @@ int main(){
 		player.armor = 0;
 		player.mp = 15;
 		player.maxmp = 15;
-	};
 
-	magic cast;{
-		cast.fireball = 5;		//fireball damage
-		cast.fireball_cost = 5;		//fireball mp cost
-		cast.healing_touch = 10;	//healing touch damage
-		cast.healing_touch_cost = 5;	//healing touch mp cost
-		cast.ward = 3;			//ward amount added to armor
-		cast.ward_cost = 3;		//ward mp cost
-	};
+	magic spellbook;
+		spellbook.fireball = 5;			//fireball damage
+		spellbook.fireball_cost = 5;		//fireball mp cost
+		spellbook.healing_touch = 10;		//healing touch damage
+		spellbook.healing_touch_cost = 5;	//healing touch mp cost
+		spellbook.ward = 3;			//ward amount added to armor
+		spellbook.ward_cost = 3;		//ward mp cost
+
 	cout << "You run into a monster perpare for a fight." << endl;
 	mob = encounter();
 	while (game != false){
@@ -64,7 +63,7 @@ int main(){
 		cout << "1 - Attack \n";
 		cout << "2 - Defend \n";
 		cout << "3 - Magic \n";
-		cout << "4 - Run \n";
+		cout << "4 - Run (This doesn't work don't use) \n";
 		cout << "************************************* \n";
 
 		cin >> choice;
@@ -76,101 +75,72 @@ int main(){
 			break;
 		case 2://defend option the same as ward spell for now//
 			if (player.defense < player.maxdefense){
-				player.defense = cast.ward + player.defense;
+				player.defense = spellbook.ward + player.defense;
 				cout << "You prepare for the imcoming attack \n";
 			}
 			else{
-				cout << "Your defense would go any higher \n";
+				cout << "Your defense would go any higher. \n";
 			}
 			break;
 		case 3://spell option//
-			cout << "What Spell do you want to use  \n";
+			cout << "************************************* \n";
+			cout << "What Spell do you want to use.  \n";
 			cout << "1 - Fireball \n";
 			cout << "2 - Healing Touch \n";
 			cout << "3 - Ward \n";
 			cout << "4 - <- Back \n";
+			cout << "************************************* \n";
 
 			cin >> spell;
 			switch (spell){ //start of spell submenu//
 			case 1://fireball spell//
-				if (player.mp >= cast.fireball_cost){
-					player.mp = player.mp - cast.fireball_cost;
-					damage = cast.fireball;
-					cout << "You shot a fireball from you hand \n";
-					cout << "You hit for " << damage << " points of damage \n";
+				if (player.mp >= spellbook.fireball_cost){
+					player.mp = player.mp - spellbook.fireball_cost;
+					damage = spellbook.fireball;
+					cout << "You shot a fireball from your hand. \n";
+					cout << "You hit for " << damage << " points of damage. \n";
 					mob.hp = mob.hp - damage;
 				}
 				else{
-					cout << "You dont have enough MP to cast this spell. \n";
+					cout << "You don't have enough MP to cast this spell. \n";
 				}
 				break;
-			case 2://healing spell need to fix hp overflow//
-				//need to add limit how much it heals//
-				if (player.mp >= cast.healing_touch_cost){
-					if (player.hp < player.maxhp){
-						player.mp = player.mp - cast.healing_touch_cost;
-						player.hp = cast.healing_touch + player.hp;
-						cout << "You restore " << cast.healing_touch << " points of health \n";
-					}
-					else{
-						cout << "You already have full HP \n";
-					}
-				}
-				else{
-					cout << "You dont have enough MP to cast this spell. \n";
-				}
+			case 2://Healing Touch spell
+				healing(spellbook.healing_touch_cost, player.mp, spellbook.healing_touch, player.maxhp, player.hp);
 				break;
-			case 3://ward spell need to fix defense overflow//
-				if(player.mp >= cast.ward_cost){
-					if(player.defense < player.maxdefense){
-						player.mp = player.mp - cast.ward_cost;
-						player.defense = cast.ward + player.defense;
-						cout << "A magic arua surronds you. \n";
-					}
-					else{
-						cout << "Your defense would go any higher \n";
-					}
-				}
-				else{
-					cout << "You dont have enough MP to cast this spell. \n";
-				}
+			case 3://Ward spell
+				ward(spellbook.ward_cost, player.mp, spellbook.ward, player.maxdefense, player.defense);
 				break;
-			case 4://back to first menu need to implement//
+			case 4://back to first menu need to implement
 				break;
-			}//end of spell submenu//
-		case 4://run away option//
-			cout << "You managed to run away" << endl;
-			game = false;
-			break;
-		case 5://secret end program//
-			cout << "Secret shut down operation activated";
-			game = false;
-			break;
+			default:
+				break;
+			}//end of spell submenu
 		default:
-			cout << "Not a valid choice. \n";
-			cout << "Choose again. \n";
 			break;
-		}//end of player switch//
+		}//end of player switch
 
-		//code runs if the monster died but player still lives//
+		//code runs if the monster died but player still lives
 		if (mob.hp <0 && player.hp >= 1){
 			cout << "You defeated the monster" << endl;
 			game = false;
 		}
-		//code runs if mob has hp and player still has hp//
+		//code runs if mob has hp and player still has hp
 		else if (game = true && mob.hp > 0 && player.hp > 0){
-			// call to the fucntion that calculates combat when mob attacks//
+			// call to the fucntion that calculates combat when mob attacks
 			player.hp = combat_system_mob(mob.attack, player.armor, player.hp, player.defense, player.maxhp);
 			if(player.hp <= 0){
 				cout << "You have been defeated." << endl;
 				game = false;
 			}
 		}
-		//code runs if player dies and mob still has hp//
+		//code runs if player dies and mob still has hp
 		else if (player.hp <= 0 && mob.hp >= 1){
 			cout << "You have been defeated. \n";
 			game = false;
 		}
 	}//end of game loop
+	cout << endl << "This Chapter is over." << endl;
+	cin >> damage;
 	return 0;
 }//end of main
